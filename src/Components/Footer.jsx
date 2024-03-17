f0f3c892a7f872984e7a87341c9f92197c51c384
Minh Dinh <macotontai123@gmail.com>
2024-03-17 14:31:31 +0700

    add Contact component, replace contact section with Contact component in Home.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,46 +1,46 @@
 export default function Footer() {
     return (
-        <div class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-7">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="footer-contact">
+        <div className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-7">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="footer-contact">
                                     <h2>Our Address</h2>
                                     <p>
-                                        <i class="fa fa-map-marker-alt"></i>FPT
-                                        University HCMC
+                                        <i className="fa fa-map-marker-alt"></i>
+                                        FPT University HCMC
                                     </p>
                                     <p>
-                                        <i class="fa fa-phone-alt"></i>+098 765
-                                        4321
+                                        <i className="fa fa-phone-alt"></i>+098
+                                        765 4321
                                     </p>
                                     <p>
-                                        <i class="fa fa-envelope"></i>
+                                        <i className="fa fa-envelope"></i>
                                         birthdayparty@gmail.com
                                     </p>
-                                    <div class="footer-social">
+                                    <div className="footer-social">
                                         <a href="">
-                                            <i class="fab fa-twitter"></i>
+                                            <i className="fab fa-twitter"></i>
                                         </a>
                                         <a href="">
-                                            <i class="fab fa-facebook-f"></i>
+                                            <i className="fab fa-facebook-f"></i>
                                         </a>
                                         <a href="">
-                                            <i class="fab fa-youtube"></i>
+                                            <i className="fab fa-youtube"></i>
                                         </a>
                                         <a href="">
-                                            <i class="fab fa-instagram"></i>
+                                            <i className="fab fa-instagram"></i>
                                         </a>
                                         <a href="">
-                                            <i class="fab fa-linkedin-in"></i>
+                                            <i className="fab fa-linkedin-in"></i>
                                         </a>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="footer-link">
+                            <div className="col-md-6">
+                                <div className="footer-link">
                                     <h2>Quick Links</h2>
                                     <a href="">Terms of use</a>
                                     <a href="">Privacy policy</a>
@@ -52,8 +52,8 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div class="copyright">
-                <div class="container">
+            <div className="copyright">
+                <div className="container">
                     <p>
                         Copyright &copy; <a href="/">Birthday Party For Kids</a>
                         , All Right Reserved.
